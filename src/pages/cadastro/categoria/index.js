cbfde4780e6668853f2b6d32cee8733cfe33652c
Ronald <ronald.serrao@trt16.jus.br>
2020-08-06 22:30:30 -0300

    Commit de fim da aula 02

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,17 +1,63 @@
 import React from 'react';
-import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
-    return (
-        <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
 
-            <Link to="/" >
-                Ir para a Home
-            </Link>
-        </PageDefault>
-    )
+      <form>
+        <div>
+            <label>
+            Nome da Categoria:
+            <input
+                type="text"
+            />
+            </label>
+        </div>
+
+        <div>
+            <label>
+            Observaçao da Categoria:
+            <input
+                type="text"
+            />
+            </label>
+        </div>
+
+        <div>
+            <label>
+            Cor:
+            <input
+                type="text"
+            />
+            </label>
+        </div>
+
+        <div>
+            <label>
+            Código de Segurança:
+            <input
+                type="text"
+            />
+            </label>
+        </div>
+        
+        <button>
+          Salvar
+        </button>
+        <button>
+          Limpar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
 }
 
 export default CadastroCategoria;
